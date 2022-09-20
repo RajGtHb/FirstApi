bb0b25363297145b7cdb377b11611a7beb91422b
RajGtHb <raj.indrajeet17@gmail.com>
2022-09-20 15:18:01 +0530

    Add project files.

---
+++ Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookLibraryEFCore;
+using BookLibraryEFCore.Models;
+
+namespace FirstApi.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
